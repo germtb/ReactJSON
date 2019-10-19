0b6cd22d400abeff5c57222544beff1863f627d5
Gerard Moreno-Torres Bertran <gerardmtb@fb.com>
2019-10-20 00:31:03 +0100

    cleanup repo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from "react";
-
 import ReactJSON from "./ReactJSON";
-import State from "./State";
 
-const root = {};
-
-ReactJSON.mount(<State />, root, () => {
-  console.log(root);
-});
+export default ReactJSON;
